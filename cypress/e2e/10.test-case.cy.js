61c1c62b936ad891e2193f04124539de80e9992e
cameronmcnz <jurgapet@gmail.com>
2025-05-12 08:03:37 +0300

    sms

--- cypress/e2e/10.test-case.cy.js
+++ cypress/e2e/10.test-case.cy.js
@@ -1,7 +1,7 @@
 /// <reference types = "cypress" />
 
-describe('template spec', () => {
-    it('passes', () => {
+describe('Verify Subscription in home page', () => {
+    it('should verify subscription in home page', () => {
 
         // 1. Launch browser
         // 2. Navigate to url 'http://automationexercise.com'
@@ -9,9 +9,21 @@ describe('template spec', () => {
         cy.visitAndVerifyHomePage();
 
         // 4. Scroll down to footer
-        // 5. Verify text 'SUBSCRIPTION'
-        // 6. Enter email address in input and click arrow button
-        // 7. Verify success message 'You have been successfully subscribed!' is visible
+        cy.get('#footer').scrollIntoView();
 
+        // 5. Verify text 'SUBSCRIPTION'
+        cy.get('.single-widget h2')
+            .should('have.text', 'Subscription')
+            .and('be.visible');
+
+        // 6. Enter email address in input and click arrow button
+        cy.get('#susbscribe_email').type('rightemail@example.com');
+        cy.get('#subscribe').click();
+
+        // 7. Verify success message 'You have been successfully subscribed!' is visible
+        cy.get('#success-subscribe')
+        .invoke('text')
+        .should('be.visible')
+        .and('include', 'You have been successfully subscribed!')
     })
 })
